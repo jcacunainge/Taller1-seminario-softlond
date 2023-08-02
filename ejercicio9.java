e68c3fce93501f6ebc899a3b90d58fbbe5a50b2e
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-08-01 21:47:19 -0500

    ejercicio9 terminado

--- ejercicio9.java
+++ ejercicio9.java
@@ -1,10 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class ejercicio9 {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
 
         // Escribe un programa que solicite al usuario un número entero positivo y
         // calcule su factorial.
 
+        String input = JOptionPane.showInputDialog("Ingrese un numero entero");
+        int numero = Integer.parseInt(input);
 
+        int resultado = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            resultado *= i; // Multiplica el resultado por el valor del contador (i)
+        }
+
+        JOptionPane.showMessageDialog(null, "El factorial de " + numero + " es: " + resultado);
     }
-    
 }
